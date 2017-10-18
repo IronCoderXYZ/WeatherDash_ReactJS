8b67e4653f807c0f32c84741d44f47253bbcd379
Mitch Hankins <mitchhankins92@gmail.com>
2017-10-18 17:20:37 -0600

    Updated searchbar component

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange({target}) {
+    console.log(target.value);
+    this.setState({ term: target.value })
+  }
+
   render() {
     return (
       <form className='input-group'>
-        <input />
+        <input
+          placeholder='Search for a city to add it to the Dashboard!'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className='input-group-btn'>
           <button type='submit' className='btn btn-secondary'>Search</button>
         </span>
